6080c8b91c67e330c91719e0a79908c5ad7e8b82
Luke-A-C-Roberts <lrobertt44@gmail.com>
2023-12-01 22:14:31 +0000

    reducing solution size

--- Day1Task1.py
+++ Day1Task1.py
@@ -11,38 +11,27 @@ spelled_numbers = [
 ]
 spelled_numbers_r = [*map(lambda x: x[::-1], spelled_numbers)]
 
+
 def find_first_number(l: str, r: bool):
     l = l[::-1] if r else l
     spelled = spelled_numbers_r if r else spelled_numbers
-    s = re.search(
-        '|'.join(char_numbers)
-        + '|'
-        + '|'.join(spelled),
-        l
-    )
-    return s
+    return re.search('|'.join(char_numbers) + '|' + '|'.join(spelled), l)
+
+
+def convert_to_int(m: re.Match, r: bool) -> int:
+    spelled = spelled_numbers_r if r else spelled_numbers
+    if m.group(0) in spelled:
+        return spelled.index(m.group(0)) + 1
+    return int(m.group(0))
+
 
 def solve(l: str) -> int:
-    print(l)
-    r1 = find_first_number(l, False)
-    r2 = find_first_number(l, True)
+    [m1, m2] = [*map(lambda b: find_first_number(l, b), [False, True])]
 
-    if r1.span() == tuple([len(l) - r2.end(), len(l) - r2.start()]):
-        if r1.group(0) in spelled_numbers:
-            return 11 * (spelled_numbers.index(r1.group(0)) + 1)
-        return 11 * int(r1.group(0))
+    if [*m1.span()] == [len(l) - m2.end(), len(l) - m2.start()]:
+        return 11 * convert_to_int(m1, False)
 
-    if r1.group(0) in spelled_numbers:
-        r1 = spelled_numbers.index(r1.group(0)) + 1
-    else:
-        r1 = int(r1.group(0))
-    if r2.group(0) in spelled_numbers_r:
-        r2 = spelled_numbers_r.index(r2.group(0)) + 1
-    else:
-        r2 = int(r2.group(0))
+    return 10 * convert_to_int(m1, False) + convert_to_int(m2, True)
 
-    return 10 * r1 + r2
 
-# print(solve("bfqpb9"))
 print(sum(map(solve, lines)))
-
